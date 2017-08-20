d1232d6959dba82bf170933b702b6d1f8e748c76
IwonaJarkiewicz <politea@gmail.com>
2017-08-20 10:18:36 +0200

    mnożeniew

--- js/function.js
+++ js/function.js
@@ -1,9 +1,25 @@
 'use strict'
 
-function multiply(param1, param2, param3) {
-    var result = param1 * param2 * param3;
-    return result;
+function multiply(param1) {
+    var iloczyn = 1;
+   
+if (param1.length != 0) {
+    for(var i =0; i < param1.length; i++){
+    iloczyn *= param1[1];
 }
+}else{
+    iloczyn = 0;
+}
+var tablica = [];
 
-var wynik = multiply(3, 9, 10);
-console.log("wynik = " + wynik);
+
+
+//var wynik = multiply(3, 9, 10);
+//console.log("wynik = " + wynik);
+
+
+var funkcja = function() {
+    return "wynik:"
+}
+//console.log(funkcja);
+//console.log(funkcja());
---
+++ js/mnozenie.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function mnozenie(tablicaLiczb) {
+    var iloczyn = 1;
+    tablicaLiczb.forEach(
+        function (element, index) {
+            iloczyn *= element;
+        }
+    );
+
+    //element - dany element w tablicy, na którym obecnie znajduje się pętla (dla pierwszego obiegu to pierwszy element, dla drugiego obiegu drugi element)
+
+    //index - indeks danego elementu w tablicy, na którym obecnie znajduje się pętla (dla pierwszego elementu jest to index 0)
+    return iloczyn;
+}
+
+var tablica = [4, 5, 10, 8];
+console.log(mnozenie(tablica));
